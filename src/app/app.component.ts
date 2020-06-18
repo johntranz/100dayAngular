037bc7347ae0582ea9a967d8a0c9e1c27e6cdbf6
HuyTran <quanghuy2621998@gmail.com>
2020-06-18 18:02:28 +0700

    day3: data binding + event binding + two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name = 'Angular' + VERSION.major;
   title = 'angular100day';
+
+  inputType = 'text';
+
+  user = {
+    name:'Huy Tran',
+    age: 22
+  };
+
+  handler(event) {
+    console.log("Click !", event);
+  }
 }
